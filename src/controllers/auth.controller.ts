5ca8fc8f6a599d4a559d9b7a423dc5dbfba0baa0
Renato Galvão <renatopgalvao@gmail.com>
2026-03-24 10:30:44 -0300

    Fix issue in found user ID to update

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -28,16 +28,24 @@ export const getProfile: RequestHandler = async (req, res) => {
 
 export const updateUser: RequestHandler = async (req, res) => {
     const { id } = req.params;
-    const loggedUser = req.user?.id;
+    const user = await UserService.getUserById(id as string);
+    if (!user) {
+        return res.status(404).json({ success: false, message: 'Usuário não encontrado' });
+    }
+    const loggedUserId = req.user?.id;
     const loggedUserRole = req.user?.role;
     const data = updateUserSchema.parse(req.body);
-    if (id !== loggedUser) {
-        console.log('os ids sao diferentes');
-        return res.status(403).json({ success: false, message: "Usuário não autorizado!" });
-    }
-    if (loggedUserRole !== 'ADMIN') {
-        console.log('não é ADMIN');
-        return res.status(403).json({ success: false, message: "Usuário não autorizado!" });
+    /*     if (id !== loggedUser) {
+            console.log('os ids sao diferentes');
+            return res.status(403).json({ success: false, message: "Usuário não autorizado!" });
+        }
+        if (loggedUserRole !== 'ADMIN') {
+            console.log('não é ADMIN');
+            return res.status(403).json({ success: false, message: "Usuário não autorizado!" });
+        } */
+
+    if (loggedUserRole !== 'ADMIN' && loggedUserId !== id) {
+        return res.status(403).json({ success: false, messsage: 'Usuário sem permissão' });
     }
 
     if (typeof id !== 'string') {
@@ -45,7 +53,7 @@ export const updateUser: RequestHandler = async (req, res) => {
     }
     const updatedUser = await UserService.updateUser(id, data);
     if (!updatedUser) {
-        return res.status(400).json({ success: false, data: { message: 'E-mail já está cadastrado. Faça o login' } });
+        return res.status(400).json({ success: false, message: 'E-mail já está cadastrado. Faça o login' });
     }
     res.status(200).json({ success: true, data: updatedUser });
 };
